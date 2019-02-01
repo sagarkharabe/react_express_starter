0d68f802cbd6bcdee6d51f06c58e2af82b5e73fc
RaghunathVadlakonda <sagarkharabe97@gmail.com>
2019-02-01 11:28:03 +0530

    fixed npm run dev script

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get('/api/customers',(req,res) => {
     const customers = [
-        {id: 1, firstName: 'John', lastName: 'Doe'},
+        {id: 1, firstName: 'Sagar', lastName: 'Kharbe'},
         {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-        {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+        {id: 3, firstName: 'Tarun', lastName: 'Rao'},
       ];
     res.json(customers);
 });
